ea88025755fb7df0f77d1e32d79c27b6e6428f1e
John Benton <jbenton4494@gmail.com>
2025-03-29 22:13:57 -0400

    basic lwe working

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let _ = print_endline "Hello World!"
+let a = [3; 4; 5] in
+let secret = [6; 7; 8] in 
+let msg = 4 in
+let noise = 2 in
+
+let cypher = Lwe.encrypt msg a secret noise in
+let original_msg = Lwe.decrypt cypher secret in
+
+Printf.printf "%d\n" original_msg
--- lib/lwe.ml
+++ lib/lwe.ml
@@ -1,4 +1,4 @@
-type t = int * int
+type t = int list * int
 
 let dot = List.fold_left2 (fun s x y -> s + x * y) 0
 
@@ -16,16 +16,14 @@ let round_power_of_2 n exp =
   let initial_round = Float.round ((Int.to_float n) /. (Int.to_float x)) in
   x * (Float.to_int initial_round)
 
-
-
 let encrypt m s a noise = 
   let product = (dot a s) + (m lsl 28) in
   let obs_product = product + noise in
   (a, obs_product)
 
 let decrypt cypher secret = 
-  let a = match cypher with |(a, b) ->  a in 
-  let obs = match cypher with |(a, b) -> b in
+  let a = match cypher with |(_, b) ->  b in 
+  let obs = match cypher with |(a, _) -> a in
   let obs_plaintext = a - (dot secret obs) in
-  Base.round_nearest obs_plaintext 2
+  (round_power_of_2 obs_plaintext 28) lsr 28
 
